ec44e645959f406a5122b6485ffad15a91aad8d5
jdittert <jondittert@gmail.com>
2023-05-23 12:27:15 -0400

    Set up education and experience displays

--- src/components/display.js
+++ src/components/display.js
@@ -3,12 +3,50 @@ import React, {Component} from "react";
 class Display extends Component {
     
     render() {
-        const {name} = this.props.personal;
+        const {personal} = this.props;
+        const {education} = this.props;
+        const {experience} = this.props;
 
         return (
             <div>              
-                <div>Display Side</div>
-                <div>{name}</div>
+                <div>
+                    <div>PERSONAL</div>
+                    <div>{personal.name}</div>
+                    <div>{personal.email}</div>
+                    <div>{personal.phone}</div>
+                    <div>{personal.address}</div>
+                    <div>{personal.website}</div>
+                </div>
+                <div>
+                    <div>EDUCATION</div>
+                    <div>
+                        <ul>
+                           {education.map((entry) => {
+                                return <li key={entry.id}>
+                                    <div>{entry.school}</div>
+                                    <div>{entry.start} - {entry.end}</div>
+                                    <div>{entry.degree}</div>
+                                    </li>
+                            })
+                        }                    
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div>EXPERIENCE</div>
+                    <div>
+                        <ul>
+                            {experience.map((entry) => {
+                                return <li key={entry.id}>
+                                    <div>{entry.company}</div>
+                                    <div>{entry.position}</div>
+                                    <div>{entry.tasks}</div>
+                                    <div>{entry.expstart} - {entry.expend}</div>
+                                    </li>
+                            })}
+                        </ul>
+                    </div>
+                </div>
             </div>
         )};
     
--- src/components/general.js
+++ src/components/general.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import '../styles/App.css'
 import '../styles/FormSide.css'
 import Display from './display';
+import uniqid from 'uniqid';
 
 
 class General extends Component {
@@ -9,31 +10,68 @@ class General extends Component {
         super(); 
 
         this.state = {
-            input: '',
             personal: {
-                name: ''
-            }
+                name: '',
+                email: '',
+                phone: '',
+                address: '',
+                website: ''
+            },
+            education: [{school: 'test', start: '2000', end: '2003', degree: 'test', id: uniqid()}],
+            experience: [{company: 'test', id: uniqid()}]
         }
-    }   
-
-    handleChange = (e) => {
-        this.setState({
-            input: e.target.value
-        });
-    };
+    }
 
     onSubmitGeneral = (e) => {
         e.preventDefault();
         const personalForm = e.target;
-        console.log(personalForm);
         const personalData = new FormData(personalForm); 
         const personalJson = Object.fromEntries(personalData.entries());
         this.setState({
             personal: {
-                name: this.state.input
+                name: personalJson.name,
+                email: personalJson.email,
+                phone: personalJson.phone,
+                address: personalJson.address,
+                website: personalJson.website                
             }
         });
-        console.log(personalJson);
+    }
+
+    onSubmitEducation = (e) => {
+        e.preventDefault();
+        const educationForm = e.target;
+        const educationData = new FormData(educationForm);
+        const educationJson = Object.fromEntries(educationData.entries());
+        const eduEntry = {
+            school: educationJson.school,
+            start: educationJson.start,
+            end: educationJson.end,
+            degree: educationJson.degree,
+            id: uniqid()
+        };
+        this.setState({
+            education: this.state.education.concat(eduEntry)
+        });
+        console.log(this.state);
+    }
+
+    onSubmitExperience = (e) => {
+        e.preventDefault();
+        const experienceForm = e.target;
+        const experienceData = new FormData(experienceForm);
+        const experienceJson = Object.fromEntries(experienceData.entries());
+        const expEntry = {
+            company: experienceJson.company,
+            position: experienceJson.position,
+            tasks: experienceJson.tasks,
+            start: experienceJson.expstart,
+            end: experienceJson.expend,            
+            id: uniqid()
+        };
+        this.setState({
+            experience: this.state.experience.concat(expEntry)
+        });
         console.log(this.state);
     }
 
@@ -42,29 +80,77 @@ class General extends Component {
         return (
             <div className='wrapper'>
                 <div className='form-section'>
-                    <div>General Info</div>
-                    <form onSubmit={this.onSubmitGeneral}
-                    id='personal-form'>
-                    <div className='input-field'>
-                        <input
-                            onChange={this.handleChange}
-                            type='text'
-                            id='name'
-                            name='name'
-                        />
-                        <label htmlFor='name'>Name</label>
+                    <div className="personal-info">
+                        <div>General Info</div>
+                        <div className="personal-form">
+                            <form onSubmit={this.onSubmitGeneral}
+                            id='personal-form'>
+                                <Input field='name' />
+                                <Input field='email' />
+                                <Input field='phone' />
+                                <Input field='address' />
+                                <Input field='website' />
+                            <div>
+                                <button type='submit'>Submit</button>
+                            </div>
+                            </form>
+                        </div>
                     </div>
-                    <div>
-                        <button type='submit'>Submit</button>
+                    <div className='education'>
+                        <div>Education</div>
+                        <div className='education-form'>
+                            <form onSubmit={this.onSubmitEducation}
+                            id='education-form'>
+                                <Input field='school' />
+                                <Input field='start' />
+                                <Input field='end' />
+                                <Input field='degree' />
+                            <div>
+                                <button type='submit'>Submit</button>
+                            </div>
+                            </form>
+                        </div>
                     </div>
-                    </form>
+                    <div className='experience'>
+                        <div>Experience</div>
+                        <div className='experience-form'>
+                            <form onSubmit={this.onSubmitExperience}
+                            id='experience-form'>
+                                <Input field='company' />
+                                <Input field='position' />
+                                <Input field='tasks' />
+                                <Input field='expstart' />
+                                <Input field='expend' />
+                            <div>
+                                <button type='submit'>Submit</button>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
                 <div>
-                    <Display personal={this.state.personal} />
+                    <Display personal={this.state.personal} education={this.state.education} experience={this.state.experience} />
                 </div>
             </div>
         );
     }
+}
+
+function Input (props) {
+    const {field} = props;
+    const fieldLabel = field.toUpperCase();
+     
+    return (
+        
+        <div className='input-field'>
+            <input
+                type='text'
+                id={field}
+                name={field}
+            />
+            <label htmlFor={field}>{fieldLabel}</label>
+        </div>
+    );
 }
 
 export default General;
