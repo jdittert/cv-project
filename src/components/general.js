918fad44b37f21811f98c3c4d9f6893239e5c443
jdittert <jondittert@gmail.com>
2023-05-25 19:18:00 -0400

    Added functionality for editing edu and exp entries

--- src/components/general.js
+++ src/components/general.js
@@ -80,7 +80,6 @@ class General extends Component {
     editSchool = (e) => {
         e.preventDefault();
         const {school} = e.target.dataset; 
-        console.log(this.state.education);
         let schools = [...this.state.education];
         let edited = {...schools[school]};
         edited.edit = true;
@@ -88,7 +87,38 @@ class General extends Component {
         this.setState({
             education: schools
         });
-        console.log(this.state.education);
+    }
+
+    cancelEditSchool = (e) => {
+        e.preventDefault();
+        const {school} = e.target.dataset; 
+        let schools = [...this.state.education];
+        let edited = {...schools[school]};
+        edited.edit = false;
+        schools[school] = edited;
+        this.setState({
+            education: schools
+        });
+    }
+
+    onUpdateEducation = (e) => {
+        e.preventDefault();
+        const {school} = e.target.dataset;        
+        let schools = [...this.state.education];
+        let edited = {...schools[school]};
+        const educationForm = e.target;
+        const educationData = new FormData(educationForm);
+        const educationJson = Object.fromEntries(educationData.entries());
+        edited.school = educationJson.school;
+        edited.start = educationJson.start;
+        edited.end = educationJson.end;
+        edited.degree = educationJson.degree;
+        edited.edit = false;
+        edited.id = uniqid();        
+        schools[school] = edited;        
+        this.setState({
+            education: schools
+        });        
     }
 
     addExperience = (e) => {
@@ -129,6 +159,51 @@ class General extends Component {
         return userExp;
     }
 
+    editCompany = (e) => {
+        e.preventDefault();
+        const {company} = e.target.dataset; 
+        let companies = [...this.state.experience];
+        let edited = {...companies[company]};
+        edited.edit = true;
+        companies[company] = edited;
+        this.setState({
+            experience: companies
+        });
+    }
+
+    cancelEditCompany = (e) => {
+        e.preventDefault();
+        const {company} = e.target.dataset; 
+        let companies = [...this.state.experience];
+        let edited = {...companies[company]};
+        edited.edit = false;
+        companies[company] = edited;
+        this.setState({
+            experience: companies
+        });
+    }
+
+    onUpdateExperience = (e) => {
+        e.preventDefault();
+        const {company} = e.target.dataset; 
+        let companies = [...this.state.experience];
+        let edited = {...companies[company]};
+        const experienceForm = e.target;
+        const experienceData = new FormData(experienceForm);
+        const experienceJson = Object.fromEntries(experienceData.entries());
+        edited.company = experienceJson.company;
+        edited.position = experienceJson.position;
+        edited.tasks = experienceJson.tasks;
+        edited.start = experienceJson.start;
+        edited.end = experienceJson.end;
+        edited.edit = false;        
+        edited.id = uniqid();
+        companies[company] = edited;                
+        this.setState({
+            experience: companies
+        });
+    }
+
     render() {
 
         return (
@@ -159,9 +234,9 @@ class General extends Component {
                             {this.educationEdits().length === 0 ? '' : 
                             <>
                             {this.state.education.map((entry) => {
-                                return <div>
-                                    {entry.edit === true ? <EducationForm onCancel={this.cancelEducation} onSubmit={this.onSubmitEducation}
-                                    values={entry} /> :
+                                return <div>                                    
+                                    {entry.edit === true ? <EducationPreFill onCancel={this.cancelEditSchool} onSubmit={this.onUpdateEducation}
+                                    values={entry} index={this.state.education.indexOf(entry)}/> :
 
                                 <div className='edit-list'>
                                     <div key={entry.id}>
@@ -193,14 +268,19 @@ class General extends Component {
                             {this.experienceEdits().length === 0 ? '' : 
                             <>
                             {this.state.experience.map((entry) => {
-                                return <div className='edit-list'>
-                                    <div>
-                                    {entry.company}
-                                    </div>
-                                    <div>
-                                    <button
-                                    data-company={this.state.experience.indexOf(entry)}>EDIT</button>
-                                    </div>
+                                return <div>
+                                    {entry.edit === true ? <ExperiencePreFill onCancel={this.cancelEditCompany} onSubmit={this.onUpdateExperience}
+                                    values={entry} index={this.state.experience.indexOf(entry)}/> :
+                                    <div className='edit-list'>
+                                        <div key={entry.id}>
+                                        {entry.company}
+                                        </div>
+                                        <div>
+                                        <button
+                                        data-company={this.state.experience.indexOf(entry)}
+                                        onClick={this.editCompany}>EDIT</button>
+                                        </div>
+                                    </div>}
                                 </div>
                             })}
                             </>
@@ -268,6 +348,36 @@ function EducationForm (props) {
     )
 }
 
+function EducationPreFill (props) {
+    const {onSubmit} = props;
+    const {onCancel} = props;
+    const {values} = props;
+    const {index} = props;
+    return (
+        <div className='education-form'>
+            <form onSubmit={onSubmit}
+            id='education-form'
+            data-school={index}>
+                <div className='education-inputs'>
+                    <Input field='school' values={values}/>
+                    <div className='date-inputs'>
+                        <Input field='start' values={values}/>
+                        <Input field='end' values={values}/>
+                    </div>
+                    <Input field='degree' values={values}/>
+                </div>
+            <div className='submit-button'>
+                <button type='submit'
+                data-school={index}>SUBMIT</button>
+                <button className='cancel'
+                onClick={onCancel}
+                data-school={index}>CANCEL</button>
+            </div>
+            </form>
+        </div>
+    )
+}
+
 function ExperienceForm (props) {
     const {onSubmit} = props;
     const {onCancel} = props;
@@ -288,6 +398,37 @@ function ExperienceForm (props) {
                 <button type='submit'>SUBMIT</button>
                 <button onClick={onCancel}
                 className='cancel'>CANCEL</button>
+            </div>
+            </form>
+        </div>
+    )
+}
+
+function ExperiencePreFill (props) {
+    const {onSubmit} = props;
+    const {onCancel} = props;
+    const {values} = props;
+    const {index} = props;
+    return (        
+        <div className='experience-form'>
+            <form onSubmit={onSubmit}
+            id='experience-form'
+            data-company={index}>
+                <div className='form-inputs'>
+                    <Input field='company' values={values} />
+                    <Input field='position' values={values} />
+                    <Input field='tasks' values={values} />
+                    <div className='date-inputs'>
+                        <Input field='start' values={values} />
+                        <Input field='end' values={values} />
+                    </div>
+                </div>
+            <div className='submit-button'>
+                <button type='submit'
+                data-company={index}>SUBMIT</button>
+                <button onClick={onCancel}
+                className='cancel'
+                data-company={index}>CANCEL</button>
             </div>
             </form>
         </div>
